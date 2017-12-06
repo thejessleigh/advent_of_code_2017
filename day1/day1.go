07346a713c1b4b349f4a888610b6b57f0e7106bc
thejessleigh <j.l.unrein@gmail.com>
2017-12-05 18:26:54 -0600

    where did that extra file come from???

--- day1/day1.go
+++ day1/day1.go
@@ -5,12 +5,6 @@ package main
 // that match the next digit in the list. The list is circuluar.
 // The digit after the last digit is the first digit in the list.
 
-// 1122 produces a sum of 3 (1 + 2) because the first digit (1)
-// matches the second digit and the third digit (2) matches the fourth digit.
-// 1111 produces 4 because each digit (all 1) matches the next.
-// 1234 produces 0 because no digit matches the next.
-// 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
-
 import (
     "fmt"
     "strconv"
