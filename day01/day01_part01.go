7f94853392ed3bb474fea73bcb3bba4d2c297a5c
thejessleigh <j.l.unrein@gmail.com>
2017-12-06 11:55:27 -0600

    go fmt day01_part01

--- day01/day01_part01.go
+++ day01/day01_part01.go
@@ -6,47 +6,46 @@ package main
 // The digit after the last digit is the first digit in the list.
 
 import (
-    "fmt"
-    "strconv"
-    "strings"
-    "container/ring"
-    "io/ioutil"
+	"container/ring"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
-
-func captcha(x string) (sum int){
-    list := strings.Split(x, "")
-    r := ring.New(len(list))
-    for i := 0; i < r.Len(); i++ {
-        num, err := strconv.Atoi(list[i])
-        if err != nil {
-            panic(err)
-        }
-        r.Value = num
-        r = r.Next()
-    }
-    sum = 0
-    for i := 0; i < r.Len(); i++ {
-        if r.Value == r.Prev().Value {
-            sum += r.Value.(int)
-        }
-        r = r.Next()
-    }
-    return
+func captcha(x string) (sum int) {
+	list := strings.Split(x, "")
+	r := ring.New(len(list))
+	for i := 0; i < r.Len(); i++ {
+		num, err := strconv.Atoi(list[i])
+		if err != nil {
+			panic(err)
+		}
+		r.Value = num
+		r = r.Next()
+	}
+	sum = 0
+	for i := 0; i < r.Len(); i++ {
+		if r.Value == r.Prev().Value {
+			sum += r.Value.(int)
+		}
+		r = r.Next()
+	}
+	return
 }
 
 func main() {
-    // testing input from problem description
-    fmt.Println(captcha("1122") == 3)
-    fmt.Println(captcha("1111") == 4)
-    fmt.Println(captcha("1234") == 0)
-    fmt.Println(captcha("91212129") == 9)
+	// testing input from problem description
+	fmt.Println(captcha("1122") == 3)
+	fmt.Println(captcha("1111") == 4)
+	fmt.Println(captcha("1234") == 0)
+	fmt.Println(captcha("91212129") == 9)
 
-    // actual puzzle input
-    raw_input, err := ioutil.ReadFile("part_one_input.txt")
-    if err != nil {
-        panic(err)
-    }
-    string_input := string(raw_input)
-    fmt.Println(captcha(string_input))
+	// actual puzzle input
+	raw_input, err := ioutil.ReadFile("part_one_input.txt")
+	if err != nil {
+		panic(err)
+	}
+	string_input := string(raw_input)
+	fmt.Println(captcha(string_input))
 }
